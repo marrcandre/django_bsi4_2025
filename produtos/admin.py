f1abe78ad5d1d557e3ece086e4919758adc01651
Marco André Mendes <marcoandre@gmail.com>
2025-09-04 09:49:49 -0300

    feat: #8 criação da model de Produtos

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Produto
 
-# Register your models here.
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome", "categoria", "preco", "estoque")
+    search_fields = ("nome", "categoria")
+    list_filter = ("criado_em", "atualizado_em")
--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    estoque = models.IntegerField(default=0)
+    preco = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+    categoria = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return f'({self.id}) {self.nome} | {self.estoque} | R$ {self.preco}'
