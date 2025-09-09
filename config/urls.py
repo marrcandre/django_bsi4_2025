375248713ea477decbec94937bddf415f0e78129
Marco André Mendes <marcoandre@gmail.com>
2025-09-09 11:05:11 -0300

    feat: instalação e configuração do Swagger

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 from rest_framework.routers import DefaultRouter
 from produtos.views import ProdutoViewSet
 
 router = DefaultRouter()
-router.register(r'produtos', ProdutoViewSet)
+router.register(r"produtos", ProdutoViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    # Rota para gerar o schema (JSON do OpenAPI)
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Swagger UI
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    # Redoc
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
